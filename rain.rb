52e135e7ff7fb71ba43a86b5608e9a445df8aa41
Manav Singh <manavjotsingh17@gmail.com>
2024-06-01 21:28:30 -0700

    added modworm link

--- rain.rb
+++ rain.rb
@@ -1,7 +1,7 @@
 class Rain < Formula
     desc "command-line utility for managing modbus servers"
     homepage "https://modworm.com"
-    url "http://localhost:5173/rain_v1.0.0.zip"
+    url "https://modworm.com/rain_v1.0.0.zip"
     version "1.0.0"
     sha256 "fb65bf3bc657891f6d0533ebebd802ae8d9b3aca66f61cc67cd0882f96c15bcf"
     license "CC-BY-ND-4.0"
