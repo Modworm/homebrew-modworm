53e04a494f013b6eae36b792d6992b77c041d31f
Manav Singh <manavjotsingh17@gmail.com>
2024-06-07 11:32:33 -0700

    added soil

---
+++ soil.rb
@@ -0,0 +1,12 @@
+cask "my-app" do
+    version "1.0.0"
+    sha256 "250f7e9e9c4c7f93cbba8644988f0bf2cb7d93568d799aed789ffd63b9385916"
+  
+    url "localhost:5173/soil_v1.0.0.dmg"
+    name "soil"
+    desc "a simple app for reading from modbus servers"
+    homepage "https://modworm.com"
+  
+    app "soil.app"
+  end
+  
