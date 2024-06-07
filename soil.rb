7590de57b6f777154849a95f13162300072f4dfb
Manav Singh <manavjotsingh17@gmail.com>
2024-06-07 12:35:31 -0700

    added soil

---
+++ soil.rb
@@ -0,0 +1,12 @@
+class Soil < Formula
+    desc "a simple app for reading from modbus servers"
+    homepage "https://modworm.com"
+    url "https://modworm.com/soil_v1.0.0.zip"
+    version "1.0.0"
+    sha256 "c583d2724cdcb6f1d96259e1f5c5a0656dc1d8de843a80c7cf29fa76d4ebdf28"
+    license "CC-BY-ND-4.0"
+  
+    def install
+      bin.install "soil"
+    end
+  end
