2717d42955b292b647411b534d10806005b384cd
Manav Singh <manavjotsingh17@gmail.com>
2024-07-06 11:13:33 -0700

    old old hash

--- soil.rb
+++ soil.rb
@@ -4,7 +4,7 @@ class Soil < Formula
     url "http://localhost:5173/soil_v2.0.0.zip"
     version "2.0.0"
     # sha256 "db14d9fe2ad102878a92303a8854eb8ed8e6948e0a24221b5b3b896c0275a3b4"
-    sha256 "c583d2724cdcb6f1d96259e1f5c5a0656dc1d8de843a80c7cf29fa76d4ebdf28"
+    sha256 "802e97c0c8d638302e8a5a28d1a72ab21bd300f65ba0cd8f6ce24efe19afe025"
     license "CC-BY-ND-4.0"
   
     def install
