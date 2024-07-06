b6da8595872d83e89652115d87cf1af4ae2f12c1
Manav Singh <manavjotsingh17@gmail.com>
2024-07-06 11:09:39 -0700

    added symlink

--- soil.rb
+++ soil.rb
@@ -2,12 +2,12 @@ class Soil < Formula
     desc "a simple app for reading from modbus servers"
     homepage "https://modworm.com"
     url "http://localhost:5173/soil_v2.0.0.zip"
-    version "1.0.0"
-    sha256 "802e97c0c8d638302e8a5a28d1a72ab21bd300f65ba0cd8f6ce24efe19afe025"
+    version "2.0.0"
+    sha256 "db14d9fe2ad102878a92303a8854eb8ed8e6948e0a24221b5b3b896c0275a3b4"
     license "CC-BY-ND-4.0"
   
     def install
-      libexec.install Dir["*"] # This copies all files in the zip to libexec
-      bin.write_exec_script (libexec/"soil") # This creates a wrapper in bin that calls your executable
+      libexec.install Dir["*"]
+      bin.install_symlink libexec/"soil"
     end
   end
