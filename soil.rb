cb72cb07d9b912c3b24ea43902b22fdf08833715
Manav Singh <manavjotsingh17@gmail.com>
2024-07-06 18:13:08 -0700

    new hash

--- soil.rb
+++ soil.rb
@@ -3,7 +3,7 @@ class Soil < Formula
     homepage "https://modworm.com"
     url "http://localhost:5173/soil_v2.0.0.zip"
     version "2.0.0"
-    sha256 "fcf37b4495047563554334e6537a87e4d05cb5a37d8ec3b39a6fdb003fb06070"
+    sha256 "bacd0f2588c3ed68e2cd7c07d530cf077250f34188d772d38957efb6cbfb2e6a"
     license "CC-BY-ND-4.0"
   
     def install
